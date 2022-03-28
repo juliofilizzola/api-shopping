118a7ecf0371397f441840270e28331a8f5093c6
Filizzola <juliofilizzola@hotmail.com>
2022-03-28 11:35:40 -0300

    🐛 : conflitos finais

--- src/shared/http/middleware/errorAPP.ts
+++ src/shared/http/middleware/errorAPP.ts
@@ -1,16 +1,22 @@
 import AppError from '../../errors/AppErros';
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 
-function errorAPP(error:Error, req: Request, res: Response, next: NextFunction) {
+function errorAPP(
+  error: Error,
+  _req: Request,
+  res: Response,
+  next: NextFunction,
+): Response<any, Record<string, any>> | undefined {
+  const errorStatus = 500;
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       status: 'error',
       message: error.message,
     });
   } else if (!(error instanceof AppError)) {
-    return res.status(500).json({
+    return res.status(errorStatus).json({
       status: 'error',
-      message: 'Internal server error'
+      message: 'Internal server error',
     });
   }
 
@@ -18,4 +24,3 @@ function errorAPP(error:Error, req: Request, res: Response, next: NextFunction) 
 }
 
 export default errorAPP;
-
