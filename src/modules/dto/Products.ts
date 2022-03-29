cb93f33706a2b0b4009490f2623fcbb578710ed6
Filizzola <juliofilizzola@hotmail.com>
2022-03-29 09:08:28 -0300

    ⚡️ : criando atualização e find de products

--- src/modules/dto/Products.ts
+++ src/modules/dto/Products.ts
@@ -3,3 +3,10 @@ export interface IProducts {
   quantity: number;
   price: number;
 }
+
+export interface IProductsUpdate {
+  id: string;
+  name: string;
+  quantity: number;
+  price: number;
+}
--- src/modules/produts/services/ShowProductService.ts
+++ src/modules/produts/services/ShowProductService.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppErros';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductsRepository } from '../typeorm/repositories/ProductsRepository';
@@ -6,6 +7,11 @@ class ShowProductService {
   public async execute(id: string): Promise<Product | undefined> {
     const produtsRepositor = getCustomRepository(ProductsRepository);
     const product = await produtsRepositor.findOne({ id });
+
+    if (!product) {
+      throw new AppError('Product not found');
+    }
+
     return product;
   }
 }
