8313ac417375627c7a2703bf3a602564f8472c55
Filizzola <juliofilizzola@hotmail.com>
2022-04-03 19:22:49 -0300

    ➕ : criando criptação de senha

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,5 +1,6 @@
 import { IUser } from '@modules/dto/User';
 import AppError from '@shared/errors/AppErros';
+import { hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/Users';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -8,11 +9,12 @@ class CreateUserService {
   public async execute({ name, email, password }: IUser): Promise<User> {
     const usersRepository = getCustomRepository(UsersRepository);
     const userExists = await usersRepository.findEmail(email);
+
     if (userExists) {
       throw new AppError('There is already email!');
     }
-
-    const bPassword = password.concat('essa é a senha');
+    const rounds = 16;
+    const bPassword = await hash(password, rounds);
 
     const user = usersRepository.create({
       password: bPassword,
