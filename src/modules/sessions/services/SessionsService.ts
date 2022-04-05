99d964021609535578c738a7b1dd288e79ab268e
Filizzola <juliofilizzola@hotmail.com>
2022-04-05 16:09:27 -0300

    🚚 : Modificando controller e service do Sessions

---
+++ src/modules/sessions/services/SessionsService.ts
@@ -0,0 +1,42 @@
+import auth from '@config/auth';
+import { IRequestLogin, IResponseLogin } from '@modules/dto/User';
+import statusCode from '@modules/utils/statusCode';
+import AppError from '@shared/errors/AppErros';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import { getCustomRepository } from 'typeorm';
+import UsersRepository from '../../users/typeorm/repositories/UsersRepository';
+
+class SessionsServicies {
+  public async execute({
+    email,
+    password,
+  }: IRequestLogin): Promise<IResponseLogin> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect Datas', statusCode.unauthorized);
+    }
+
+    const passwordConfirmed = await compare(password, user.password);
+
+    if (!passwordConfirmed) {
+      throw new AppError('Incorrect Datas', statusCode.unauthorized);
+    }
+
+    const secret = auth.jwt.secret || '';
+
+    const token = sign({ user: user.id }, secret, {
+      subject: user.id,
+      expiresIn: auth.jwt.expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default SessionsServicies;
