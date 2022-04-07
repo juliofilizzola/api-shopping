f7621629ac5a0e94f7801751887a7239e3a73879
Filizzola <juliofilizzola@hotmail.com>
2022-04-07 11:50:26 -0300

    📦 : finalizando multer

--- src/shared/http/app/app.ts
+++ src/shared/http/app/app.ts
@@ -7,11 +7,13 @@ import 'express-async-errors';
 import router from '../routes/index.routes';
 import errorAPP from '../middleware/errorAPP';
 import '@shared/typeorm';
+import upload from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(upload.directory));
 app.use(router);
 app.use(errors());
 app.use(errorAPP);
